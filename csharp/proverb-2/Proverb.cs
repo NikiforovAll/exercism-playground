ed248f9606205b290216621e9e465f3fa0cdaf2e
NikiforovAll <nikiforovalekcey@gmail.com>
2020-10-05 16:15:22 +0000

    Add protein-translation

---
+++ csharp/proverb-2/Proverb.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects) =>
+        subjects.Any() ? subjects.Zip(subjects.Skip(1))
+            .Select(z => $"For want of a {z.First} the {z.Second} was lost.")
+            .Append($"And all for the want of a {subjects.First()}.")
+            .ToArray() : Array.Empty<string>();
+}
