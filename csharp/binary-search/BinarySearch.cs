df63c7865d585f712037c338dfc49e1504b447e5
NikiforovAll <nikiforovalekcey@gmail.com>
2020-10-13 20:19:20 +0000

    Add binary-search

---
+++ csharp/binary-search/BinarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+public static class BinarySearch
+{
+    public static int Find(int[] input, int value) =>
+        BFind(input, value);
+
+    private static int BFind<T>(ReadOnlySpan<T> source, T value)
+        where T : IComparable
+    {
+        if (source.Length is 0)
+            return -1;
+
+        var mid_i = source.Length / 2;
+        var mid = source[mid_i];
+        return mid.CompareTo(value) switch
+        {
+            0 => mid_i,
+            1 when BFind(source[..mid_i], value) is var ind && ind != 1
+                => ind,
+            -1 when BFind(source[(mid_i + 1)..], value) is var ind && ind != -1
+                => mid_i + 1 + ind,
+            _ => -1
+        };
+    }
+
+}
--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -4,16 +4,16 @@ using System.Linq;
 
 public class GradeSchool
 {
-    private readonly SortedList<(string n, int g), (string n, int g)> _roaster =
-        new SortedList<(string n, int g), (string n, int g)>(
+    private readonly SortedSet<(string n, int g)> _roaster =
+        new SortedSet<(string n, int g)>(
         Comparer<(string n, int g)>.Create(
             (x, y) => x.g != y.g
                 ? Comparer<int>.Default.Compare(x.g, y.g)
                 : Comparer<string>.Default.Compare(x.n, y.n)));
-    public void Add(string student, int grade) => _roaster.Add((student, grade), (student, grade));
+    public void Add(string student, int grade) => _roaster.Add((student, grade));
 
-    public IEnumerable<string> Roster() => _roaster.Values.Select(entry => entry.n);
+    public IEnumerable<string> Roster() => _roaster.Select(entry => entry.n);
 
     public IEnumerable<string> Grade(int grade) =>
-        _roaster.Keys.Where(entry => entry.g == grade).Select(entry => entry.n);
+        _roaster.Where(entry => entry.g == grade).Select(entry => entry.n);
 }
